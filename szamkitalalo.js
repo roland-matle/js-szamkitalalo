8adcb0d3328186df9fb604310d9763ee6530252a
Roland <matleroland@gmail.com>
2024-04-10 17:55:19 +0200

    alert + tipp kinyerese

--- szamkitalalo.js
+++ szamkitalalo.js
@@ -1,11 +1,12 @@
 let guessListContainer = document.querySelector(".js-guess-container");
 let currentGuessField = document.querySelector(".js-guess-field");
 let newGameButton = document.querySelector(".js-new-game-button");
+let guessSubmitButton = document.querySelector(".js-submit-guess");
 
 let target;
 
 function generateTarget() {
-    return Math.floor(Math.random()*100)+1;
+    return Math.floor(Math.random() * 100) + 1;
 }
 
 function resetGame() {
@@ -20,7 +21,20 @@ function startGame() {
     target = generateTarget();
 }
 
+function submitGuess(event) {
+    event.preventDefault();
+    let guessValue = Number.parseInt(currentGuessField.value);
+    console.log(guessValue, typeof guessValue);
+    if (Number.isNaN(guessValue)) {
+
+    } else if (guessValue < 1 || guessValue > 100) {
+
+    }
+    alert("klikk");
+}
+
 startGame();
 newGameButton.addEventListener("click", startGame);
+guessSubmitButton.addEventListener("click", submitGuess);
 
-console.log(target)
+console.log(target);
